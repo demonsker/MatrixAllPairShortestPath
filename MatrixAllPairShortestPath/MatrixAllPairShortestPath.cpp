f6d49f2278d30f8b3e4cc4336c7e66025bae3268
ITIS <57050194@kmitl.ac.th>
2018-03-03 21:40:02 +0700

    Change tempDistance to RowMajor

--- MatrixAllPairShortestPath/MatrixAllPairShortestPath.cpp
+++ MatrixAllPairShortestPath/MatrixAllPairShortestPath.cpp
@@ -12,7 +12,7 @@ void distance_generate(int[][SIZE]);
 void distance_useexample(int[][SIZE]);
 void find_AllPairShortestPath(int[][SIZE], int[][SIZE]);
 void array_print(int[][SIZE]);
-void array_cpy(int[][SIZE], int[][SIZE]);
+void array_transpose(int[][SIZE], int[][SIZE]);
 void log_save(float);
 
 int main()
@@ -71,16 +71,18 @@ void find_AllPairShortestPath(int distance[][SIZE], int path[][SIZE])
 	
 	for (int r = 0; r < round; r++)
 	{
-		array_cpy(distance, temp_distance);
+		//Use inverse copy for row major
+		array_transpose(distance, temp_distance);
 		for (int i = 0; i < SIZE; i++)
 		{
 			for (int j = 0; j < SIZE; j++)
 			{
 				for (int k = 0; k < SIZE; k++)
 				{
-					if (distance[i][k] + temp_distance[k][j] < distance[i][j])
+					//Use inverse (j,k) for row major
+					if (distance[i][k] + temp_distance[j][k] < distance[i][j])
 					{
-						distance[i][j] = distance[i][k] + temp_distance[k][j];
+						distance[i][j] = distance[i][k] + temp_distance[j][k];
 						path[i][j] = path[i][k];
 					}
 				}
@@ -108,7 +110,7 @@ void distance_generate(int data[][SIZE])
 	}
 }
 
-void array_cpy(int sour[][SIZE], int dest[][SIZE])
+void array_transpose(int sour[][SIZE], int dest[][SIZE])
 {
 	int i, j;
 
@@ -116,7 +118,7 @@ void array_cpy(int sour[][SIZE], int dest[][SIZE])
 	{
 		for (j = 0; j < SIZE; j++)
 		{
-			dest[i][j] = sour[i][j];
+			dest[i][j] = sour[j][i];
 		}
 	}
 }
